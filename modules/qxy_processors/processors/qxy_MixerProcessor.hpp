0b64c28cb87fc83573958a8507e36fb6c428726c
Sam Borkent <sam_borkent@hotmail.com>
2023-02-13 13:42:50 +0100

    feat: added processor module with MixerProcessor

---
+++ modules/qxy_processors/processors/qxy_MixerProcessor.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+namespace qxy
+{
+
+    template <class InnerProcessor>
+    class MixerProcessor : public InnerProcessor
+    {
+    public:
+        void prepare (const juce::dsp::ProcessSpec& spec)
+        {
+            InnerProcessor::prepare (spec);
+            dryWetMixer.prepare (spec);
+        }
+
+        template <class ProcessContext>
+        void process (const ProcessContext& context)
+        {
+        }
+
+        template <class ProcessContext>
+        void mixWetSamples (const ProcessContext& context)
+        {
+            dryWetMixer.mixWetSamples (context.getOutputBlock());
+        }
+
+        template <class ProcessContext>
+        void pushDrySamples (const ProcessContext& context)
+        {
+            dryWetMixer.pushDrySamples (context.getInputBlock());
+        }
+
+        void setWetMixProportion (float wetMixProportion)
+        {
+            dryWetMixer.setWetMixProportion (wetMixProportion);
+        }
+
+        void setMixingRule (juce::dsp::DryWetMixingRule mixingRule)
+        {
+            dryWetMixer.setMixingRule (mixingRule);
+        }
+
+    private:
+        juce::dsp::DryWetMixer<float> dryWetMixer;
+        
+    };
+   
+} // namespace qxy
---
+++ modules/qxy_processors/qxy_processors.h
@@ -0,0 +1,32 @@
+/*
+==============================================================================
+
+BEGIN_JUCE_MODULE_DECLARATION
+
+   ID:            qxy_processors
+   vendor:        Qxy
+   version:       0.0.1
+   name:          Qxy Processors
+   description:   Collection of DSP Processors
+   dependencies:
+
+   website:       https://github.com/samborkent/qxy-modules
+   license:       BSD 3-Clause
+
+   END_JUCE_MODULE_DECLARATION
+
+==============================================================================
+*/
+
+#pragma once
+
+#include <juce_dsp/juce_dsp.h>
+
+#include "processors/qxy_MixerProcessor.hpp"
+
+namespace qxy
+{
+
+
+   
+} // namespace qxy
