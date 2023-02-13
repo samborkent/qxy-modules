1b33e1b2795966f58fef60df43c32b76dcb83f9d
Sam Borkent <sam_borkent@hotmail.com>
2023-02-13 10:46:47 +0100

    fix: fixed module structure

---
+++ modules/qxy_simd/qxy_simd.h
@@ -0,0 +1,23 @@
+/*
+==============================================================================
+
+BEGIN_JUCE_MODULE_DECLARATION
+
+   ID:            qxy_simd
+   vendor:        Qxy
+   version:       0.0.1
+   name:          Qxy SIMD
+   description:   SIMD code
+   dependencies:
+
+   website:       https://github.com/samborkent/qxy-modules
+   license:       BSD 3-Clause
+
+   END_JUCE_MODULE_DECLARATION
+
+==============================================================================
+*/
+
+#include <juce_dsp/juce_dsp.h>
+
+#include "simd/qxy_Simd.hpp"
