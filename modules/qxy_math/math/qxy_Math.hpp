78c003482a292f21015e52687db30eff84ef50c8
Sam Borkent <sam_borkent@hotmail.com>
2023-02-13 13:31:17 +0100

    feat: added math module

---
+++ modules/qxy_math/math/qxy_Math.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+namespace qxy
+{
+    namespace math
+    {
+
+        template <typename Type>
+        inline Type floor (Type val)
+        {
+            return static_cast<Type>(static_cast<int>(val));
+        }
+
+        template <typename Type>
+        inline Type randomGaussian (Type mean, Type std)
+        {
+            static Type t = Type(0);
+            Type x, w1, w2, r;
+
+            if (t == Type(0))
+            {
+                do
+                {
+                    w1 = random (Type(-1), Type(1));
+                    w2 = random (Type(-1), Type(1));
+                    r = w1 * w1 + w2 * w2;
+                }
+                while (r >= Type(1));
+
+                r = std::sqrt (Type(-2) * std::log (r) / r);
+                t = w2 * r;
+                return mean + w1 * r * std;
+            }
+            else
+            {
+                x = t;
+                t = Type(0);
+                return mean + x * std;
+            }
+        }
+
+        template <typename Type>
+        inline Type round (Type val)
+        {
+            return std::nearbyint (val);
+        }
+
+        template <typename Type>
+        inline Type sgn (Type val)
+        {
+            return (val > Type(0)) - (val < Type(0));
+        }
+
+    } // namespace math
+} // namespace qxy
---
+++ modules/qxy_math/qxy_math.h
@@ -0,0 +1,33 @@
+/*
+==============================================================================
+
+BEGIN_JUCE_MODULE_DECLARATION
+
+   ID:            qxy_math
+   vendor:        Qxy
+   version:       0.0.1
+   name:          Qxy Math
+   description:   Math functions
+   dependencies:
+
+   website:       https://github.com/samborkent/qxy-modules
+   license:       BSD 3-Clause
+
+   END_JUCE_MODULE_DECLARATION
+
+==============================================================================
+*/
+
+#pragma once
+
+#include "math/qxy_Math.hpp"
+
+namespace qxy
+{
+
+   namespace math
+   {
+
+   } // namespace math
+   
+} // namespace qxy
